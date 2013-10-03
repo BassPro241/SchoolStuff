2fb20642209304afa464565fb33f7ea0c106ce58
BassPro241 <crashal96@aol.com>
2013-10-03 14:06:59 -0400

    Shapes Program.

---
+++ Eclipse/Stuff/src/shapes/Stuff.java
@@ -0,0 +1,28 @@
+package shapes;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+public class Stuff {
+	
+	public static void main(String[] args)
+	{
+		
+		JFrame frame1, frame2;
+		int choice1 = JOptionPane.showConfirmDialog(
+			    frame1,
+			    "Would you like to calculate the area of a shape?",
+			    "Shapes",
+			    JOptionPane.YES_NO_OPTION);
+		
+		if(choice1 == 1)
+		{
+			do
+			{
+				
+			}while()
+		}
+	}
+
+}
